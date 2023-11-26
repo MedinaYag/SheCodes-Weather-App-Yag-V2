266a0364855c2f6248ba3e3f3fd08da9655d0b18
MedinaYag <152028924+MedinaYag@users.noreply.github.com>
2023-11-26 18:28:35 +0100

    HTML & JS & CSS

---
+++ source.yag/index.js
@@ -0,0 +1,47 @@
+let now = new Date();
+let currentDate = document.querySelector("#current-date");
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+weather.innerHTML = `${day}, ${hours}:${minutes}`;
+function Search(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-input");
+
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  searchCity(city);
+}
+
+function searchCity(city) {
+  let apiKey = "1a747f2d7ac32a100bt13fab8776o6ca";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
